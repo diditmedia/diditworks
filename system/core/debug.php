a3baf0f621337dbabaa8b94abf799528d7bfd7d1
Paul Mulgrew <paul@diditmedia.com>
2012-05-30 19:03:49 -0400

    Added Support for logging.
    Added a logging setting in the front controller to define the logging level
    
    0    - Do not log anything
    1    - Log only errors
    2    - Log errors and database transactions
    3    - Log errors, database transactions and debugging
    4    - Log everything
    
    to support this there the debug class in system/core has been implemented with a static method log

--- system/core/debug.php
+++ system/core/debug.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * -----------------------------------------------------------
  * DEBUG
@@ -14,7 +15,35 @@ class debug
 {
 
 
-        //put your code here
+        static function log($message, $level)
+        {
+
+                if($level <= LOG && LOG != 0)
+                {
+                        //define the path for the logs to be stored
+                        $logpath = APP_PATH . SEP . 'logs';
+
+                        //chefk if the log directory exists and if it doesnt create it
+                        if(!is_dir($logpath))
+                        {
+                                mkdir($logpath, 0777);
+                        }
+
+                        //define the log file
+                        $file = $logpath . '/log.txt';
+
+                        //open the file for appending
+                        $w = fopen($file, 'a');
+
+                        //write the message to the file 
+                        fwrite($w, date('d.m.y h:i:s') . ' ' . $message . ' ' . 'FILE: ' . __FILE__ . ' LINE: ' . __LINE__ . PHP_EOL);
+
+                        //close the file
+                        fclose($w);
+                }
+
+        }
+
 
 }
 
