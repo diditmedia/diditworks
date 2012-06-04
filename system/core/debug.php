5ca722d0f2f7b7546b76c1bcaa25815b0766ea35
Paul Mulgrew <paul@diditmedia.com>
2012-06-04 09:43:06 -0400

    bug fix - check if directory is writable

--- system/core/debug.php
+++ system/core/debug.php
@@ -17,39 +17,39 @@ class debug
 
         static function log($message, $level)
         {
-
-                if($level <= LOG && LOG != 0)
+                if(is_writable(APP_PATH))
                 {
-                        //define the path for the logs to be stored
-                        $logpath = APP_PATH . SEP . 'logs';
-
-                        //chefk if the log directory exists and if it doesnt create it
-                        if(!is_dir($logpath))
+                        if($level <= LOG && LOG != 0)
                         {
-                                mkdir($logpath, 0755);
+                                //define the path for the logs to be stored
+                                $logpath = APP_PATH . SEP . 'logs';
+
+                                //chefk if the log directory exists and if it doesnt create it
+                                if(!is_dir($logpath))
+                                {
+                                        mkdir($logpath, 0755);
+                                }
+
+                                //create a directory for the current month/year
+                                if(!is_dir($logpath . SEP . date('M-Y')))
+                                {
+                                        mkdir($logpath . SEP . date('M-Y'), 0755);
+                                }
+
+                                $logpath = $logpath . SEP . date('M-Y');
+
+                                //define the log file
+                                $file = $logpath . '/' . date('D-j') . '.txt';
+
+                                //open the file for appending
+                                $w = fopen($file, 'a');
+
+                                //write the message to the file 
+                                fwrite($w, date('d.m.y h:i:s') . ' ' . $message . PHP_EOL);
+
+                                //close the file
+                                fclose($w);
                         }
-                        
-                        //create a directory for the current month/year
-                        if(!is_dir($logpath.SEP.date('M-Y')))
-                        {
-                                mkdir($logpath.SEP.date('M-Y'), 0755);
-                                
-                                
-                        }
-                        
-                        $logpath = $logpath.SEP.date('M-Y');
-
-                        //define the log file
-                        $file = $logpath . '/'.date('D-j').'.txt';
-
-                        //open the file for appending
-                        $w = fopen($file, 'a');
-
-                        //write the message to the file 
-                        fwrite($w, date('d.m.y h:i:s') . ' ' . $message .  PHP_EOL);
-
-                        //close the file
-                        fclose($w);
                 }
 
         }
